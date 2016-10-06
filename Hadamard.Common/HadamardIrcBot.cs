915d50fbdd293c36d5a15def56af8d605f5dbbac
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2016-10-06 21:04:02 +0200

    Will now track satellite 25338 upon command request.

--- Hadamard.Common/HadamardIrcBot.cs
+++ Hadamard.Common/HadamardIrcBot.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using IrcDotNet;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace Hadamard.Common
 {
@@ -14,7 +16,6 @@ namespace Hadamard.Common
 
         public HadamardIrcBot() : base()
         {
-
             Connect("orwell.freenode.net", RegistrationInfo);
             _commandProcessors = CommandProcessors;
         }
@@ -72,8 +73,35 @@ namespace Hadamard.Common
         protected override void OnChannelMessageReceived(IrcChannel channel, IrcMessageEventArgs e)
         {
             base.OnChannelMessageReceived(channel, e);
-            channel.Client.LocalUser.SendMessage(channel.Name, e.Text.GetHashCode().ToString());
+
+            if(e.Text.Equals("satinfo"))
+            {
+                using (var webClient = new WebClient())
+                {
+                    var response = webClient.DownloadString("http://www.n2yo.com/sat/instant-tracking.php?s=25338&hlat=70.07436&hlng=29.74872&d=300&r=139203158747.09302&tz=GMT+02:00&O=n2yocom&rnd_str=5b53a06e197ed03f2075e8c1d85fa6d6");
+                    dynamic model = JsonConvert.DeserializeObject(response);
+
+                    var satellite = new
+                    {
+                        NoradId = model[0].id,
+                        Latitude = model[0].pos.First.d.ToString().Split('|')[0],
+                        Longtitude = model[0].pos.First.d.ToString().Split('|')[1]
+                    };
+                    var replyMessage = $"Current position latitude: {satellite.Latitude} longtitude: {satellite.Longtitude} norad id: {satellite.NoradId}";
+                    _client.LocalUser.SendMessage(_channel, replyMessage);
+                }
+            }
         }
     }
 
 }
+/*
+var test = responseModel[0].id;
+var pos = responseModel[0].pos;
+
+dynamic position = responseModel[0].pos;
+dynamic poss = position.First;
+
+var latitude = poss.d.ToString().Split('|')[0];
+var longtitude = poss.d.ToString().Split('|')[1];
+*/
