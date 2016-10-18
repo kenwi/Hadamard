2f23740a0ff7e198a110bf7a0606c97de9f8ec2d
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2016-10-18 19:41:39 +0200

    Testing repository

--- Hadamard.UI/Program.cs
+++ Hadamard.UI/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 using Hadamard.UI.View;
 using Hadamard.Common;
 using Hadamard.Common.Model;
@@ -23,17 +22,31 @@ namespace Hadamard.UI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            ISatelliteRepository repository = new SatelliteRepository();
-            repository.OnSatelliteValuesUpdated += (s, e) => MessageBox.Show($"Updated satellite: {e.Satellite.Id}");
-            //repository.UpdateAll();
-
+            ISatelliteRepository repository = new SatelliteRepository();            
             var allSatellites = repository.GetAllSatellites();
             var singleSatellite = repository.GetSatelliteByIndex(0);
             var selectedSatellite = repository.GetSatelliteById(25544);
-            repository.Add(new Satellite(25544));
 
+            repository.OnSatelliteValuesUpdated += (s, e) =>
+            {
+                Console.WriteLine($"Satellite '{e.Satellite.Id}' updated latitude '{e.Satellite.Latitude}' longitude '{e.Satellite.Longitude}'");
+            };
 
+            repository.OnSatelliteAdded += (s, e) =>
+            {
+                Console.WriteLine($"Satellite with ID '{e.Satellite.Id}' added");
+            };
 
+            while(true)
+            {
+                var input = Console.ReadLine();
+                if (input.Equals("show"))
+                    repository.UpdateAll();
+                else if(input.StartsWith("add"))
+                {
+                    repository.Add(new Satellite(int.Parse(input.Split(' ')[1])));
+                }
+            }
 
             //var view = new SatelliteForm();
             /*var presenter = new SatellitePresenter(view, repository);
